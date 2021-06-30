ecff3880c1b10fbc9e6af64ed4a4bfd50292c547
ziynii <85431762+ziynii@users.noreply.github.com>
2021-06-30 15:14:29 +0900

    JS: Project filtering

--- main.js
+++ main.js
@@ -53,6 +53,29 @@ arrowUp.addEventListener('click', () => {
 	scrollIntoView('#home');
 })
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+	const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+	if (filter == null) {
+		return;
+	}
+	projectContainer.classList.add('ani-out');
+	setTimeout(() => {
+		projects.forEach((project) => {
+			if (filter === '*' || filter === project.dataset.type) {
+				project.classList.remove('invisible')
+			} else {
+				project.classList.add('invisible');
+			}
+		});
+		projectContainer.classList.remove('ani-out');
+	}, 300);
+
+})
+
 
 function scrollIntoView(selector) {
 	const scrollTo = document.querySelector(selector);
