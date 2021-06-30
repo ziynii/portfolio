463a960b2e6095d9771e175c7a45e0b809a12169
ziynii <85431762+ziynii@users.noreply.github.com>
2021-06-30 11:46:32 +0900

    JS: Navbar menu click event

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ document.addEventListener('scroll', () => {
 		navbar.classList.remove('navbar--dark');
 	}
 });
+
+// Handel scrolling
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+	const target = event.target;
+	const link = target.dataset.link;
+	if (link == null) {
+		return;
+	}
+	console.log(event.target.dataset.link);
+	const scrollTo = document.querySelector(link);
+	scrollTo.scrollIntoView({behavior: 'smooth'});
+})
